48eeeaef2bcca8fd93a296ef6435ff607efa3783
Do Khanh Vi <dokhanhvi@Dos-MacBook-Air-4.local>
2026-05-13 15:08:44 +0700

    change email templates and port

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 
+
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-    </Routes>
-  );
+ return (
+   <Routes>
+     <Route path="/" element={<LoginPage />} />
+     <Route path="/login" element={<LoginPage />} />
+   </Routes>
+ );
 }
+
 
 export default App;
